a4274e21e1139a98331caca2c5b8bc7879dff622
OceanOfTruth <bahriansyahtabong@gmail.com>
2023-03-09 15:30:11 +0700

    Upload Praktikum3

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,21 @@
+<?php
+
+$domisili = ["jakarta", "bogor", "depok", "tanggerang", "bekasi"];
+$program_studi = [
+//    I  ini adalah key
+//    V
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital",
+];
+
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWD Bootstraps"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50  
+];
+?>
